2aa8985cbd15d6835626f7bd1afea28fee04bebf
David Eduardo <david.eduardoti@gmail.com>
2022-10-28 13:22:40 -0300

    Add deliver.json and new functions

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -1,78 +1,29 @@
 import singupPage from '../pages/signuPage'
 describe('Sign up', () => {
     
-    var signup = new singupPage()
-
-    before(function(){
-        cy.log('Everything here will be executed once BEFORE ALL test cases.')
-    })
-
     beforeEach(function(){
-        cy.log('Everything here will be executed everytime BEFORE ALL test cases.')
+        cy.fixture('deliver').then((d)=>{
+            this.deliver = d
+        })
     })
+   
     
-    after(function(){
-        cy.log('Everything here will be executed once AFTER ALL test cases.')
-    })
-
-    afterEach(function(){
-        cy.log('Everything here will be executed everytime AFTER ALL test cases.')
-    })
-    
-    it('Be a deliveryman', () => {
-        //Deliveryman object:
-        var deliveryman = {
-            name: 'David Eduardo',
-            cpf: '01234567895',
-            email: 'eduardo.ti@gmail.com',
-            whatsapp: '11988888888',
-            //Object within an object:
-             address:{
-                zip_code: '63024060',
-                street: 'Rua José Vitorino Sobrinho',
-                number:'0101',
-                district: 'São José',
-                landmark: 'Green house',
-                city_uf: 'Juazeiro do Norte/CE'
-            },
-
-
-            //In this case, the delivery method was Motocycle
-            delivery_method: 'Moto',
-            driving_licence: 'driving_licence.jpg'
-        }
-        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
-        
+    it('Be a deliveryman', function(){
+        var signup = new singupPage()
         signup.go()
-        signup.fillForm(deliveryman)
+        signup.fillForm(this.deliver.signup)
         signup.submit()
+        
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
         signup.modalContentShouldBe(expectedMessage)
 
     });
 
-    it('CPF not valid', () => {
-        //Deliveryman object:
-        var deliveryman = {
-            name: 'David Eduardo',
-            cpf: '012345678AA', //Change the two last numbers into letters
-            email: 'eduardo.ti@gmail.com',
-            whatsapp: '11988888888',
-            //Object within an object:
-             address:{
-                zip_code: '63024060',
-                street: 'Rua José Vitorino Sobrinho',
-                number:'0101',
-                district: 'São José',
-                landmark: 'Green house',
-                city_uf: 'Juazeiro do Norte/CE'
-            },
-            //In this case, the delivery method was Motocycle
-            delivery_method: 'Moto',
-            driving_licence: 'driving_licence.jpg'
-        }
-
+    it('CPF not valid', function(){
+      
+         var signup = new singupPage()
         signup.go()
-        signup.fillForm(deliveryman)
+        signup.fillForm(this.deliver.inv_cpf)
         signup.submit()
         signup.alertMessageShouldBe('Oops! CPF inválido')
 
--- cypress/pages/signuPage.js
+++ cypress/pages/signuPage.js
@@ -1,5 +1,6 @@
 class signupPage{
 
+ 
     go(){
         cy.viewport(1440, 900)
         cy.visit('https://buger-eats.vercel.app')
@@ -9,25 +10,25 @@ class signupPage{
     }
 
     fillForm(deliveryman){
-        cy.get('input[name="name"]').type(deliveryman.name)
-        cy.get('input[name="cpf"]').type(deliveryman.cpf)
-        cy.get('input[name="email"]').type(deliveryman.email)
-        cy.get('input[name="whatsapp"]').type(deliveryman.whatsapp)
+        cy.get('input[name="name"]').type(deliveryman?.name)
+        cy.get('input[name="cpf"]').type(deliveryman?.cpf)
+        cy.get('input[name="email"]').type(deliveryman?.email)
+        cy.get('input[name="whatsapp"]').type(deliveryman?.whatsapp)
 
         //Fill the fields referring to the deliveryman adress
-        cy.get('input[name="postalcode"]').type(deliveryman.address.zip_code)
+        cy.get('input[name="postalcode"]').type(deliveryman?.address.zip_code)
         cy.get('input[type=button][value="Buscar CEP"]').click()
 
-        cy.get('input[name="address-number"]').type(deliveryman.address.number)
-        cy.get('input[name="address-details"]').type(deliveryman.address.landmark)
+        cy.get('input[name="address-number"]').type(deliveryman?.address.number)
+        cy.get('input[name="address-details"]').type(deliveryman?.address.landmark)
         
         //Fill validation:
-        cy.get('input[name="address"]').should('have.value', deliveryman.address.street)
-        cy.get('input[name="district"]').should('have.value', deliveryman.address.district)
-        cy.get('input[name="city-uf"]').should('have.value', deliveryman.address.city_uf)
+        cy.get('input[name="address"]').should('have.value', deliveryman?.address.street)
+        cy.get('input[name="district"]').should('have.value', deliveryman?.address.district)
+        cy.get('input[name="city-uf"]').should('have.value', deliveryman?.address.city_uf)
 
-        cy.contains('.delivery-method li', deliveryman.delivery_method).click()
-        cy.get('input[accept^="image"]').attachFile(deliveryman.driving_licence)
+        cy.contains('.delivery-method li', deliveryman?.delivery_method).click()
+        cy.get('input[accept^="image"]').attachFile(deliveryman?.driving_licence)
     }
 
     submit(){
